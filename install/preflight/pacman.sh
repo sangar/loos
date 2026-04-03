74e32f81556fce8513cf724d461e0175386f48f3
Gard Sandholt <gard.sandholt@gmail.com>
2026-04-03 20:15:44 +0200

    Match both DisableSandbox and #DisableSandbox

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -1,4 +1,5 @@
 # Fix pacman sandbox issues on ARM kernel
-if ! grep -q "^DisableSandbox$" /etc/pacman.conf; then
+# Check if DisableSandbox exists (commented or not) but not DisableSandboxFilesystem/Syscalls
+if ! grep -q "^#\?DisableSandbox$" /etc/pacman.conf; then
   sudo sed -i 's/^DownloadUser = alpm$/DownloadUser = alpm\nDisableSandbox/' /etc/pacman.conf
 fi
