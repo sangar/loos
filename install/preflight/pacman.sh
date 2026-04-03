c2e2fd8031371f1bc2bfb6897cd4c0217caddab3
Gard Sandholt <gard.sandholt@gmail.com>
2026-04-03 19:47:17 +0200

    Does not create duplicate DisableSandbox entries

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -1,4 +1,4 @@
 # Fix pacman sandbox issues on ARM kernel
-if ! grep -q "DisableSandbox" /etc/pacman.conf; then
-  sudo sed -i 's/^DownloadUser = alpm$/DownloadUser = alpm\nDisableSandboxFilesystem\nDisableSandboxSyscalls/' /etc/pacman.conf
+if ! grep -q "^DisableSandbox$" /etc/pacman.conf; then
+  sudo sed -i 's/^DownloadUser = alpm$/DownloadUser = alpm\nDisableSandbox/' /etc/pacman.conf
 fi
