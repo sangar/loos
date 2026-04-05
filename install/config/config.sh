4128b43d3e79e07d6d0d8ca91293823797cdaa03
Gard Sandholt <gard.sandholt@gmail.com>
2026-04-05 19:07:50 +0200

    Set keyboard layout

--- install/config/config.sh
+++ install/config/config.sh
@@ -4,3 +4,8 @@ cp -R ~/.local/share/loos/config/* ~/.config/
 
 # Use default bashrc from LoOS
 cp ~/.local/share/loos/default/bashrc ~/.bashrc
+
+# Sync keyboard layout from vconsole to Hyprland
+if command -v loos-keyboard-sync &>/dev/null; then
+  loos-keyboard-sync
+fi
