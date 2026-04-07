f0ffaffcb618351345f850e84b6bbf0612902eb3
Gard Sandholt <gard.sandholt@gmail.com>
2026-04-07 16:54:02 +0200

    Removed Arch ARM handling

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -1,8 +1,3 @@
-# Fix pacman sandbox issues on ARM kernel
-if ! grep -q "DisableSandboxFilesystem" /etc/pacman.conf; then
-  sudo sed -i 's/^DownloadUser = alpm$/DownloadUser = alpm\nDisableSandboxFilesystem\nDisableSandboxSyscalls/' /etc/pacman.conf
-fi
-
 # Remove stale locks
 sudo rm -f /var/lib/pacman/db.lck
 
