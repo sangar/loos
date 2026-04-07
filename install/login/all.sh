7380281502d766ed2944f27ceae8f5a0e212b152
Gard Sandholt <gard.sandholt@gmail.com>
2026-04-07 16:52:51 +0200

    Added sddm config

--- install/login/all.sh
+++ install/login/all.sh
@@ -1,1 +1,2 @@
 source $LOOS_INSTALL/login/hypr.sh
+source $LOOS_INSTALL/login/sddm.sh
---
+++ install/login/sddm.sh
@@ -0,0 +1,23 @@
+# Install omarchy SDDM theme
+loos-refresh-sddm
+
+# Setup SDDM login service
+sudo mkdir -p /etc/sddm.conf.d
+if [[ ! -f /etc/sddm.conf.d/autologin.conf ]]; then
+  cat <<EOF | sudo tee /etc/sddm.conf.d/autologin.conf
+[Autologin]
+User=$USER
+Session=hyprland-uwsm
+
+[Theme]
+Current=simple
+EOF
+fi
+
+# Prevent password-based SDDM logins from creating an encrypted login keyring
+# (which conflicts with the passwordless Default_keyring used for auto-unlock)
+sudo sed -i '/-auth.*pam_gnome_keyring\.so/d' /etc/pam.d/sddm
+sudo sed -i '/-password.*pam_gnome_keyring\.so/d' /etc/pam.d/sddm
+
+# Don't use chrootable here as --now will cause issues for manual installs
+sudo systemctl enable sddm.service
