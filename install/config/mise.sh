00117333d911e56d49c3cbc5842a697f7d7b01f5
Gard Sandholt <gard.sandholt@gmail.com>
2026-04-09 18:35:15 +0200

    Removed rust

--- install/config/mise.sh
+++ install/config/mise.sh
@@ -1,8 +1,7 @@
 # Activate mise for current shell
-if command -v mise &> /dev/null; then
+if command -v mise &>/dev/null; then
   eval "$(mise activate bash)"
 fi
 
 # Install global tools via mise
 mise use -g node@latest
-mise use -g rust@latest
